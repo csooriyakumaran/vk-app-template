e70f3e37e969c9ecf72439cbef8107506b628d6e
csooriyakumaran <c.sooriyakumaran@gmail.com>
2025-04-28 11:53:18 -0400

    feat: playing with shaders

--- app/app-gui/src/app.cpp
+++ app/app-gui/src/app.cpp
@@ -6,14 +6,21 @@
 #include "vk/core/procedure.h"
 
 #include "vk/ui/console.h"
+#include "vk/render/shader.h"
+#include "vk/render/renderer.h"
 
 #include "imgui.h"
 #include "implot.h"
 
 #include "yaml-cpp/yaml.h"
+#include "glad/glad.h"
 
 #include <string_view>
 #include <format>
+
+
+static u32 s_compute_shader = -1;
+static const std::filesystem::path s_compute_shader_path = "shaders/compute.glsl";
 
 class MainProc : public vk::Procedure
 {
@@ -33,6 +40,10 @@ public:
 private:
     vk::ui::Console m_console;
     vk::ui::ConsoleLog m_log;
+
+    void draw_render_viewport();
+    vk::Texture m_texture;
+    vk::FrameBuffer m_fb;
 
 };
 
@@ -70,6 +81,57 @@ void MainProc::attach()
         LOG_INFO("exp as f32: {}", cfg["exp"].as<f32>());
         LOG_INFO("exp as f64: {}", cfg["exp"].as<f64>());
     }
+
+    s_compute_shader = vk::create_compute_shader(s_compute_shader_path);
+    ASSERT( s_compute_shader != -1, "Compute shader failed");
+
+    u32 width = vk::Application::get().get_window().get_width();
+    u32 height = vk::Application::get().get_window().get_height();
+    m_texture = vk::create_texture(width, height);
+    m_fb = vk::create_framebuffer_with_texture(m_texture);
+
+}
+
+void MainProc::draw_render_viewport()
+{
+    ImGui::Begin("Render Viewport");
+
+
+    int width = (int)ImGui::GetContentRegionAvail().x;
+    int height = (int)ImGui::GetContentRegionAvail().y;
+    
+    if (width != m_texture.width || height != m_texture.height)
+    {
+        ::glDeleteTextures(1, &m_texture.handle);
+
+        m_texture = vk::create_texture(width, height);
+        vk::attach_texture_to_framebuffer(m_fb, m_texture);
+    }
+
+    { // compute
+        ::glUseProgram(s_compute_shader);
+        ::glBindImageTexture(0, m_fb.color_attachement.handle, 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_RGBA32F);
+
+        const GLuint workgroup_size_x = 16;
+        const GLuint workgroup_size_y = 16;
+
+        GLuint num_groups_x = (width + workgroup_size_x - 1) / workgroup_size_x;
+        GLuint num_groups_y = (height + workgroup_size_y - 1) / workgroup_size_y;
+
+        ::glDispatchCompute(num_groups_x, num_groups_y, 1);
+
+        ::glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
+
+    }
+
+    { // Blit
+        // vk::blit_framebuffer_to_swapchain(m_fb);
+    }
+    
+    ImGui::Image((ImTextureID)m_texture.handle, {(float)m_texture.width, (float)m_texture.height});
+
+    ImGui::End();
+
 }
 
 void MainProc::detach()
@@ -89,12 +151,16 @@ void MainProc::render()
     m_console.render();
     m_log.render();
 
+    draw_render_viewport();
+
     ImGuiIO& io = ImGui::GetIO();
 
     if ( ImGui::IsKeyDown(ImGuiKey_ModCtrl) & ImGui::IsKeyPressed(ImGuiKey_Minus) )
         io.FontGlobalScale -= 0.1f;
     if ( ImGui::IsKeyDown(ImGuiKey_ModCtrl) & ImGui::IsKeyPressed(ImGuiKey_Equal) )
         io.FontGlobalScale += 0.1f;
+    if ( ImGui::IsKeyDown(ImGuiKey_R))
+        s_compute_shader = vk::reload_compute_shader(s_compute_shader, s_compute_shader_path);
     // vk::Application& app = vk::Application::get();
     // app.close();
 
